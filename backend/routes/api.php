eab7c72e6704414c978c43181f03fcaeeae1f314
Muhammad Taha <taha_khan198923@yahoo.com>
2021-01-24 13:22:57 +0500

    some work

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,10 +10,11 @@ use Illuminate\Http\Request;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
-|
+|q
 */
 Route::post('register', 'API\AuthController@register');
 Route::post('login', 'API\AuthController@login');
+Route::apiResource('ceo', 'API\CEOController')->middleware('auth:api');
 
 Route::group(['middleware' => ['cors', 'auth:api', 'json.response']], function () {
     // ...
